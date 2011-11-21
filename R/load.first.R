f5f10c51a474352eb444358b96cc251936045ac9
Yu-Sung Su <suyusung@tsinghua.edu.cn>
2011-11-21 00:00:00 +0000

    version 0.09-15

--- R/load.first.R
+++ R/load.first.R
@@ -2,13 +2,6 @@
   mylib <- dirname(system.file(package = "mi"))
   ver <- packageDescription("mi", lib.loc = mylib)$Version
   builddate <- packageDescription("mi", lib.loc = mylib)$Date
-  cat(paste("\nmi (Version ", ver, ", built: ", builddate, ")\n", sep = ""))
-  if(!any(search()=="package:car"))
-    require(car)
-  if(!any(search()=="package:abind"))
-    require(abind)
-  if(!any(search()=="package:car"))
-    require(car)
-  if(!any(search()=="package:arm"))
-    require(arm)
+  msg <- paste("\nmi (Version ", ver, ", built: ", builddate, ")\n", sep = "")
+  packageStartupMessage(msg, appendLF = FALSE)
 }
